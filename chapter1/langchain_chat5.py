949d50b2674716f91aae4e5df31bb071573fd199
DeepFuture <12919034+gongkaiming@user.noreply.gitee.com>
2026-02-07 10:02:49 +0800

    Revert "fix: code modify"
    This reverts commit 759886215c26d1a1e19cdcb269d80789ace598e4.

--- chapter1/langchain_chat5.py
+++ chapter1/langchain_chat5.py
@@ -2,7 +2,7 @@ import os
 
 import gradio as gr
 import requests
-from dotenv import load_dotenv
+from keyring.core import load_env
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_core.output_parsers import StrOutputParser, JsonOutputKeyToolsParser
@@ -11,13 +11,14 @@ from langchain_core.tools import tool
 
 # description: langchain工具调用示例，集成天气查询API
 
-load_dotenv()
+load_env()
+
 
 @tool
 def get_weather(location: str):
     """
         查询即时天气函数
-        :param location: 必要参数，字符串类型，用于表示查询天气的具体城市名称，\
+        :param loc: 必要参数，字符串类型，用于表示查询天气的具体城市名称，\
         :return：心知天气 API查询即时天气的结果，具体URL请求地址为："https://api.seniverse.com/v3/weather/now.json"
         返回结果对象类型为解析之后的JSON格式对象，并用字符串形式进行表示，其中包含了全部重要的天气信息
     """
@@ -44,9 +45,6 @@ model = init_chat_model(
 tools = [get_weather]
 
 llm_with_tools = model.bind_tools(tools)
-
-response = llm_with_tools.invoke("请帮我查询北京的天气情况？")
-print(response)
 
 parser = JsonOutputKeyToolsParser(key_name=get_weather.name, first_tool_only=True)
 
